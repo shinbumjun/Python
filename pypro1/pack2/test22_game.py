162deebfc09ef62420789212b5a271914b37ce4a
shinbumjun <https://github.com/shinbumjun>
2022-05-01 21:41:13 +0900

    27. 제 3자가 만들어 놓은 게임

---
+++ pypro1/pack2/test22_game.py
@@ -0,0 +1,49 @@
+'''
+[제 3자가 만들어 놓은 게임]
+
+파이 게임(무료)
+https://www.pygame.org/news.html
+C:\anaconda3\Lib\site-packages 이 안에 파이게임은 없다
+
+Python 참고문서 및 참조 사이트
+https://cafe.daum.net/flowlife/RUrO/2
+
+[1번째 방법]
+*파이썬 관련 모듈 다운로드
+http://www.lfd.uci.edu/~gohlke/pythonlibs/   
+-> 
+pygame 검색 및 클릭
+-> 
+pygame‑1.9.6‑cp39‑cp39‑win_amd64.whl 다운로드(버젼이 중요)
+-> 
+C:\\anaconda3\\lib\\site-packages
+
+[2번째 방법]
+Anaconda Prompt 실행
+바로가기(고급: 관리자 권한으로 실행(R))
+-> pip install pygame
+
+다운받기
+https://cafe.daum.net/flowlife/SBQ7/2 
+- racecar.png
+- racecar.py
+'''
+# import pygame
+# pygame.init()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
