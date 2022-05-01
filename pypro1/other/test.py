b397ea81d2469defdf21abd038d3e980fec1fcf7
shinbumjun <https://github.com/shinbumjun>
2022-05-01 21:35:02 +0900

    19. test

---
+++ pypro1/other/test.py
@@ -0,0 +1,44 @@
+'''
+while과 if문을 사용하여 아래와 같이 3, 5, 7, 9 단의 구구단을 출력하는 코드를 작성하시오
+'''
+i = 3
+while i <= 3: 
+    j = 1
+    while j <= 9:
+        print(str(i)+'*'+str(j),'=',int(i*j), end=' ')
+        j += 1
+    i += 1
+print('\n')
+i = 5
+while i <= 5: 
+    j = 1
+    while j <= 9:
+        print(str(i)+'*'+str(j),'=',int(i*j), end=' ')
+        j += 1
+    i += 1    
+print('\n') 
+i = 7
+while i <= 7: 
+    j = 1
+    while j <= 9:
+        print(str(i)+'*'+str(j),'=',int(i*j), end=' ')
+        j += 1
+    i += 1
+print('\n')
+i = 9
+while i <= 9: 
+    j = 1
+    while j <= 9:
+        print(str(i)+'*'+str(j),'=',int(i*j), end=' ')
+        j += 1
+    i += 1
+
+
+
+
+
+
+
+
+
+
