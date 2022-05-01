e3e2579e51af95b9a134f60f2dfa98f76c1af101
shinbumjun <https://github.com/shinbumjun>
2022-05-01 22:26:01 +0900

    58. 메뉴1 메뉴2 Local DB 자료 보기 - Anaconda3[아나콘다] http://127.0.0.1:8000/admin/login/?next=/admin/ diango5/123xxx

---
+++ django5_db/dbapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from dbapp.models import Article
+
+# Create your views here.
+def Main(request):
+    return render(request, 'main.html')
+
+def DbShow(request):
+    datas = Article.objects.all()       # Django ORM 사용 // select * from article 
+    print(datas, type(datas))           # class 'django.db.models.query.QuerySet'
+    print(datas[0].name)
+    
+    return render(request, 'list.html', {'articles':datas})
