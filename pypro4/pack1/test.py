e5eef30f560f68719e7d4950d356bedb219c837a
shinbumjun <https://github.com/shinbumjun>
2022-05-18 18:11:04 +0900

    120. ***단순선형회귀모델 작성방법 세 가지 경험하기

---
+++ pypro4/pack1/test.py
@@ -0,0 +1,49 @@
+# Python3 샘플 코드 #
+import pandas as pd
+import xmltodict
+import requests
+
+url = 'http://apis.data.go.kr/1360000/TourStnInfoService/getTourStnVilageFcst'
+params ={'serviceKey' : '4hfjQRMp3NUakGj6w+fTuBIKpB9zdwnZ8QzwME1OsMTNrC+d79ChoOLJiyiez+WQpuLu+vsog4mAh3J1aMTrng==', 'pageNo' : '1', 'numOfRows' : '10', 'dataType' : 'XML', 'CURRENT_DATE' : '2019122010', 'HOUR' : '24', 'COURSE_ID' : '1' }
+
+# response = requests.get(url, params=params)
+# print(response.content)
+
+
+resp = requests.get(url, params=params)
+print(resp.content.decode('utf-8'))
+print()
+data = xmltodict.parse(resp.content.decode('utf-8'))
+print(data)
+print()
+print(data['response']['body']['items']['item'])
+print()
+print(pd.DataFrame(data['response']['body']['items']['item']))
+
+
+
+# import requests
+# import xmltodict
+# import pandas as pd
+#
+# # http://apis.data.go.kr/1360000/TourStnInfoService
+# # 4hfjQRMp3NUakGj6w+fTuBIKpB9zdwnZ8QzwME1OsMTNrC+d79ChoOLJiyiez+WQpuLu+vsog4mAh3J1aMTrng==
+# url = 'http://apis.data.go.kr/1360000/TourStnInfoService'
+# params ={'serviceKey' : '4hfjQRMp3NUakGj6w+fTuBIKpB9zdwnZ8QzwME1OsMTNrC+d79ChoOLJiyiez+WQpuLu+vsog4mAh3J1aMTrng==', 'pageNo' : '1', 'numOfRows' : '10', 'dataType' : 'XML', 'CURRENT_DATE' : '2019122010', 'HOUR' : '24', 'COURSE_ID' : '1'}
+#
+# resp = requests.get(url, params=params)
+# print(resp.content.decode('utf-8'))
+# print()
+# data = xmltodict.parse(resp.content.decode('utf-8'))
+# print(data)
+# print()
+# print(data['response']['body']['items']['item'])
+# print()
+# print(pd.DataFrame(data['response']['body']['items']['item']))
+
+
+
+
+
+
+
